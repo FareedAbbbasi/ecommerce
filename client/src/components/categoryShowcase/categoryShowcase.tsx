3ac82d5faf982289bda20c9b3405ab4cef3e20fb
FareedAbbbasi <codingabbas0999@gmail.com>
2025-04-29 10:12:31 +0500

    feat: Implement the sider functionality in the website

--- client/src/components/categoryShowcase/categoryShowcase.tsx
+++ client/src/components/categoryShowcase/categoryShowcase.tsx
@@ -1,8 +1,55 @@
-// import React from 'react'
-
 import { ArrowRightIcon } from "lucide-react"
+import { useState } from "react";
+
+interface SidesProps {
+    id: number;
+    title: string
+    subtitle: string
+    image: string
+}
+
+const sides: SidesProps[] = [
+    {
+        id: 1,
+        title: "Up to 10% off Voucher",
+        subtitle: "iPhone 14 Series",
+        image: "/src/assets/products/phone.png"
+    },
+    {
+        id: 2,
+        title: "Big Deals on iPhone 13",
+        subtitle: "iPhone 13 Series",
+        image: "/src/assets/products/phone.png"
+    },
+    {
+        id: 3,
+        title: "Special Offer on iPhone 12",
+        subtitle: "iPhone 12 Series",
+        image: "/src/assets/products/phone.png",
+    },
+    {
+        id: 3,
+        title: "Special Offer on iPhone 12",
+        subtitle: "iPhone 12 Series",
+        image: "/src/assets/products/phone.png",
+    },
+    {
+        id: 3,
+        title: "Special Offer on iPhone 12",
+        subtitle: "iPhone 12 Series",
+        image: "/src/assets/products/phone.png",
+    },
+    {
+        id: 3,
+        title: "Special Offer on iPhone 12",
+        subtitle: "iPhone 12 Series",
+        image: "/src/assets/products/phone.png",
+    }
+]
+
 
 const CategoryShowcase = () => {
+    const [currentSide, setCurrentSide] = useState(0);
     const categories = [
         "Woman's Fashion",
         "Men's Fashion",
@@ -14,10 +61,11 @@ const CategoryShowcase = () => {
         "Groceries & Pets",
         "Health & Beauty"
     ]
+
     return (
         <div className='flex w-full max-w-[1170px] h-auto min-h-[344px]  mx-auto mt-[40px] mb-[50px] md:mb-[100px] lg:mb-[140px] tablet:px-5 lg:px-7 flex-wrap md:flex-nowrap'>
             <div className=" px-4 md:px-4 lg:px-0">
-                {categories.map((category, index)=> (
+                {categories.map((category, index) => (
                     <div key={index} className="pb-4">
                         <a href=""><h3 className="text-[16px]">
                             {category}
@@ -26,22 +74,30 @@ const CategoryShowcase = () => {
                 ))}
             </div>
             <div className="h-[384px] border bg-[#828282] ml-4 mr-[45px]"></div>
-            <div className="w-full flex max-w-[892px] h-auto min-h-[344px] bg-black sm:ml-0 md:ml-[45px] text-white justify-center items-center">
-                <div className=" px-5 md:px-10 flex flex-col justify-center">
-                    <div className="flex items-center gap-2 tablet:gap-[10px] md:gap-3 lg:gap-[14.5px]">
-                        <img src="/src/assets/products/logo.png" alt="" />
-                        <p className="">iPhone 14 Series</p>
-                    </div>
-                    <h1 className="w-full max-w-[294px] h-auto min-h-[120px] text-[25px] sm:text-[25px] tablet:text-[30px] md:text-[35px] lg:text-[48px] font-semibold mt-2 md:mt-4 lg:mt-5 mb-[22px]">Up to 10% off Voucher</h1>
-                    <div className="flex gap-1 md:gap-2">
-                       <a className="hover:underline decoration-solid underline-offset-[7px]" href="">Shop Now</a>
-                        <ArrowRightIcon />
-                    </div>
-                </div>
-                <div>
-                    <img src="/src/assets/products/phone.png" alt="" />
-                </div>
+            <div className="w-full max-w-[892px] h-auto min-h-[344px] bg-black">
+                <div className="flex h-auto sm:ml-0 md:ml-[45px] text-white justify-center items-center py-6">
+                    <div className=" px-5 md:px-10 bg-black flex flex-col justify-center">
+                        <div className="flex items-center gap-2 tablet:gap-[10px] md:gap-3 lg:gap-[14.5px]">
+                            <img src="/src/assets/products/logo.png" alt="" />
+                            <p className="">{sides[currentSide].subtitle}</p>
+                        </div>
+                        <h1 className="w-full max-w-[294px] h-auto min-h-[120px] text-[25px] sm:text-[25px] tablet:text-[30px] md:text-[35px] lg:text-[48px] font-semibold mt-2 md:mt-4 lg:mt-5 mb-[22px]">{sides[currentSide].title}</h1>
+                        <div className="flex gap-1 md:gap-2">
+                            <a className="hover:underline decoration-solid underline-offset-[7px]" href="">Shop Now</a>
+                            <ArrowRightIcon />
+                        </div>
 
+                    </div>
+                    <div>
+                        <img src={sides[currentSide].image} alt="" />
+                    </div>
+
+                </div>
+                <div className="flex justify-center pt-4 gap-3 mb-4">
+                    {sides.map((_,index) => (
+                    <button key={index} onClick={()=> setCurrentSide(index)} className={`w-3 h-3 rounded-full ${currentSide===index? "bg-red-500" : "bg-gray-400"}`}></button>
+                    ))}
+                </div>
             </div>
         </div>
     )
--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -5,17 +5,23 @@ import MobileNavbar from "../components/ui/mobileNavbar";
 
 export const Header:React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isInputOpen, setIsInputOpen] = useState(false)
   return (
     <div className='w-full max-w-[1170px] h-auto min-h-[38px] mx-auto mt-10 mb-4'>
-      <div className="w-full flex justify-between items-center flex-wrap px-4 xl:*:px-0">
+      <div className="w-full flex justify-between items-center flex-wrap px-1 tablet:px-2 md:px-3 lg:px-4 xl:px-0">
         <h1 className="text-[16px] sm:text-[18px] md:text-[22px] lg:text-[24px] font-bold">Exclusive</h1>
         <div className="hidden lg:block py-2 md:px-0 ">
           <Navbar />
         </div>
-        <div className="flex gap-6 ">
-          <div className="flex lg:bg-[#F5F5F5] py-[7px] px-5 md:flex">
-            <input className=" bg-transparent outline-none backdrop-blur-none text-[12px] hidden lg:block" type="text" placeholder="What is you looking for?" />
-            <Search className="w-4 h-4"/>
+        <div className="flex gap-2 tablet:gap-3 md:gap-5 lg:gap-6">
+          <div className="flex lg:bg-[#F5F5F5] py-[7px] px-2 tablet:px-3 md:px-4 lg:px-5 md:flex">
+            <input className=" bg-transparent outline-none backdrop-blur-none text-[12px] hidden lg:block" type="text" placeholder="What are you looking for?"/>
+            <Search className="w-4 h-4" onClick={()=> setIsInputOpen(!isInputOpen)}/> 
+              {
+                isInputOpen && (
+                  <input className="pl-1 bg-transparent outline-none backdrop-blur-none text-[12px] md:hidden" type="text" placeholder="What are you looking for?"/>
+                )
+              }
           </div>
           <div className="flex justify-center items-center gap-4">
             <Heart  className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6"/>
